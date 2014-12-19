3a3e7fb01e05cc9fc6427b8908a3d537f420e6c4
Daoud Clarke <daoud.clarke@gmail.com>
2014-12-19 15:24:46 +0000

    Search substrings of queries

--- fbsearch/lucenesearch.py
+++ fbsearch/lucenesearch.py
@@ -11,13 +11,21 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
 from org.apache.lucene.util import Version
 
+import Levenshtein
+
 import sys
 import re
+
+
+STOPWORDS = set("""
+  a an and are as at be but by for if in into is it
+  no not of on or such that the their then there these
+  they this to was will with who what where
+""".split())
 
 VALID_CHARS_PATTERN = re.compile('[\W_]+')
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
-
 
 
 class LuceneSearcher(object):
@@ -30,6 +38,19 @@ class LuceneSearcher(object):
         self.analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
         logger.info("Loaded DB from %s with %d documents: ",
                     db_path, reader.numDocs())
+
+    def query_search(self, query):
+        """
+        Find entities that are contained as substrings in the query.
+        """
+        query_terms = [term for term in query.split() if term not in STOPWORDS]
+        all_entities = []
+        for i in range(len(query_terms) - 1):
+            subquery = ' '.join(query_terms[i:i+2])
+            docs = self.search(subquery)
+            distances = [(Levenshtein.distance(doc['text'], unicode(subquery)), doc) for doc in docs]
+            all_entities += distances
+        return sorted(all_entities)
         
     def search(self, query):
         query = VALID_CHARS_PATTERN.sub(' ', query)
@@ -39,7 +60,7 @@ class LuceneSearcher(object):
         score_docs = self.searcher.search(query, 100).scoreDocs
         logger.debug("%s total matching documents.",
                      len(score_docs))
-
+        
         docs = [self.searcher.doc(d.doc) for d in score_docs]
         return [self.convert_to_dict(doc) for doc in docs]
         
@@ -51,7 +72,7 @@ if __name__ == '__main__':
     print 'lucene', lucene.VERSION
     path = '/home/dc/Experiments/sempre/lib/lucene/4.4/inexact/'
     searcher = LuceneSearcher(path)
-    docs = searcher.search(sys.argv[1])
+    docs = searcher.query_search(sys.argv[1])
     for doc in docs:
         print doc
 
