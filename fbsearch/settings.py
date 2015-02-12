407ee835bb90f0ce8e49de93cd8d9a831a87ffd8
Daoud Clarke <daoud.clarke@gmail.com>
2015-02-12 15:51:37 +0000

    Change some settings for hpc

--- fbsearch/settings.py
+++ fbsearch/settings.py
@@ -5,4 +5,5 @@ RESULTS_PATH = 'target_predicted.json'
 
 ORACLE_CACHE_PATH = 'oracle_cache.json'
 
-LUCENE_PATH = '/home/dc/Experiments/sempre/lib/lucene/4.4/inexact/'
+LUCENE_PATH = '/home/d/dc/dc34/scratch/sempre/lib/lucene/4.4/inexact'
+# LUCENE_PATH = '/home/dc/Experiments/sempre/lib/lucene/4.4/inexact/'
